9bf6e2bbf01a7b77b89621349d4608f76cfd16f4
chengyifan <chengyifan@taou.com>
2022-05-11 11:26:04 +0800

    使用dotenv

--- bark.ts
+++ bark.ts
@@ -1,10 +1,12 @@
+import "https://deno.land/x/dotenv/load.ts";
+
+const BARK_HOOK = Deno.env.get("BARK_HOOK") || "";
+
 export async function sendNotification(
   title: string,
   content: string,
   silence = false
 ) {
   const sound = silence ? "silence" : "glass";
-  await fetch(
-    `https://api.day.app/x4niMU4kaSwfSvbmspkEym/${title}/${content}?sound=${sound}`
-  );
+  await fetch(`${BARK_HOOK}/${title}/${content}?sound=${sound}`);
 }
--- robot.ts
+++ robot.ts
@@ -1,28 +1,27 @@
-// https://open.feishu.cn/open-apis/bot/v2/hook/55204739-71f1-4cad-ada3-ff327e65b4a1
+import "https://deno.land/x/dotenv/load.ts";
+
+const LARK_ROBOT_HOOK = Deno.env.get("LARK_ROBOT_HOOK") || "";
 
 export async function sendMessage(title: string, content: string) {
-  await fetch(
-    "https://open.feishu.cn/open-apis/bot/v2/hook/55204739-71f1-4cad-ada3-ff327e65b4a1",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        msg_type: "post",
-        content: {
-          post: {
-            zh_cn: {
-              title: title,
-              content: [
-                [
-                  {
-                    tag: "text",
-                    text: content,
-                  },
-                ],
+  await fetch(LARK_ROBOT_HOOK, {
+    method: "POST",
+    body: JSON.stringify({
+      msg_type: "post",
+      content: {
+        post: {
+          zh_cn: {
+            title: title,
+            content: [
+              [
+                {
+                  tag: "text",
+                  text: content,
+                },
               ],
-            },
+            ],
           },
         },
-      }),
-    }
-  );
+      },
+    }),
+  });
 }
